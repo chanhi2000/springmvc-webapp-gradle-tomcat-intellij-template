dcd39ad0291325c2aa152e9d9a0d0fd6b1789efd
chanhi2000 <chanhi2000@gmail.com>
2019-02-27 14:51:22 +0900

    javaSetup completed

--- src/main/java/com/spms/servlet3/WebInitializer.java
+++ src/main/java/com/spms/servlet3/WebInitializer.java
@@ -1,23 +1,23 @@
-//package com.spms.servlet3;
-//
-//import com.spms.config.RootConfig;
-//import com.spms.config.WebMvcConfig;
-//import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-//
-//
-//public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-//    @Override
-//    protected Class<?>[] getRootConfigClasses() {
-//        return new Class[] {RootConfig.class};
-//    }
-//
-//    @Override
-//    protected Class<?>[] getServletConfigClasses() {
-//        return new Class[] {WebMvcConfig.class};
-//    }
-//
-//    @Override
-//    protected String[] getServletMappings() {
-//        return new String[] { "/" };
-//    }
-//}
+package com.spms.servlet3;
+
+import com.spms.config.RootConfig;
+import com.spms.config.WebMvcConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] {RootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
